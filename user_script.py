bf88043b59b3c29015eaa19921702164f5f68117
Rodrigo Bessa <56771198+RodrigoBC2@users.noreply.github.com>
2021-04-05 22:13:13 -0300

    Created a function called prime_range(a,b) for later use of this script

--- user_script.py
+++ user_script.py
@@ -1,28 +1,34 @@
 from main import prime_number_check
 
-print("Enter 2 integer numbers. The second number must be greater than the first")
+prime_numbers_discovered = []
+
+
+def prime_range(a, b):
+
+    user_inputs = range(a, b)
+
+    for inputs in user_inputs:
+
+        prime_return = prime_number_check(inputs)
+
+        if prime_return:
+            prime_numbers_discovered.append(inputs)
+
+    return prime_numbers_discovered
+
+
+print("Enter 2 integer numbers. The first number can not be zero and the second number must be greater than the first")
 
 a = int(input())
 b = int(input())
 
-# while loop to guarantee that user enter the values in the correct way
-while a >= b:
-    print("Enter 2 integer numbers. The second number must be greater than the first")
+while a >= b or a == 0:
+
+    print("Enter 2 integer numbers. The first number can not be zero and the second number must be greater than the first")
 
     a = int(input())
-    b = int(input())
+    b = int(input()) + 1
 
-# range from the smaller to the greater number.
-user_inputs = range(a, b + 1)
+prime_range(a, b)
 
-# list to save the prime numbers discovered
-prime_numbers_discovered = []
-
-# for loop to analyze each number from a to b and save the prime numbers by adding it to the empty list
-for inputs in user_inputs:
-    prime_return = prime_number_check(inputs)
-    if prime_return == True:
-        prime_numbers_discovered.append(inputs)
-
-# printing the prime numbers discovered
 print("Numbers discovered:", prime_numbers_discovered)
