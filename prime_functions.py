2c94c40dbc6d9c33d5b01e01b146fc7e9627ba32
Rodrigo Bessa <56771198+RodrigoBC2@users.noreply.github.com>
2021-04-27 21:52:02 -0300

    Update prime_functions.py

--- prime_functions.py
+++ prime_functions.py
@@ -28,15 +28,11 @@ def prime_range(a, b):
 # function to generate prime numbers from a start number to a specific amount greater than the given one.
 def generate_primes(from_number, amount):
 
-    start = 0
     found_prime_numbers = []
 
-    while start <= amount:
-        from_number += 1
-        
+    while len(found_prime_numbers) < amount:
         if prime_number_check(from_number):
             found_prime_numbers.append(from_number)
-
-        start += 1
+        from_number += 1
 
     return found_prime_numbers
