f4063f273c7eb7499705e2ce10edcc43e2f83cca
Rodrigo Bessa <56771198+RodrigoBC2@users.noreply.github.com>
2021-05-08 17:59:16 -0300

    Update prime_functions.py

--- prime_functions.py
+++ prime_functions.py
@@ -38,6 +38,7 @@ def generate_primes(from_number, amount):
     return found_prime_numbers
 
 
+# fuction to give all prime numbers from 1 to the input number
 def eratosthenes(limit_input):
 
     prime_list = [i for i in range(1, limit_input+1)]
@@ -55,3 +56,45 @@ def eratosthenes(limit_input):
         n += 1
 
     return prime_list
+
+
+# function to return True of False for a given entry. If False, the entry number is not prime. If True, its probably prime.
+import random
+
+# If False is returned then it is false that n is prime
+# if True os returned it is probably true that n is prime
+def miller_rabin(n,k):
+
+    if n == 2:
+        return True
+
+    if n % 2 == 0:
+        return False
+
+    r = 0
+    s = 0
+    n = -1
+    
+    # This while loop is infinite
+    while s % 2 == 0:
+        r += 1
+        s //= 2
+
+    # underscore indicates that the for loop variable does not matter, just want to cycle in the loop.
+    for _ in range(k):
+        a = random.randrange(2, n - 1)
+        x = pow(a, s, n)
+
+        if x == 1 or x == n - 1:
+            continue
+
+        for _ in range(r - 1):
+            x = pow(x, 2, n)
+
+            if x == n - 1:
+                break
+
+        else:
+            return False
+
+    return True
